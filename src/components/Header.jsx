3d5bafa95ab2562e411f2489fb4d91282fbe8e9f
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-05-17 13:22:22 +0700

    adding button open&close

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useContext } from "react";
+import { SidebarContext } from "../context/SidebarContext";
 
-const Header = () => {
-  return <div>Header</div>;
-};
+function Header() {
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
+
+  return (
+    <>
+      <div>
+        <div>Header</div>
+        <div onClick={() => setIsOpen(!isOpen)}>Open/Close</div>
+      </div>
+    </>
+  );
+}
 
 export default Header;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+// import icons
+import { IoMdArrowForward } from "react-icons/io";
+import { FiTrash2 } from "react-icons/fi";
+import { useContext } from "react";
+import { SidebarContext } from "../context/SidebarContext";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
-};
+function Sidebar() {
+  const { isOpen, handleClose } = useContext(SidebarContext);
+
+  return (
+    <>
+      <div
+        className={`${
+          isOpen ? "right-0" : "-right-full"
+        } "w-full h-full bg-white fixed top-0 shadow-2xl md:w-[35vw] xl:w-[30vw] xl:max-w-[30vw] transtion-all duration-300 z-20 px-4`}>
+        Sidebar
+      </div>
+    </>
+  );
+}
 
 export default Sidebar;
