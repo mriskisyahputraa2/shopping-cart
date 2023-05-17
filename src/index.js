897e45b6c1aa70c8c7bc71323ac2e9e8c2dcf973
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-05-17 13:05:12 +0700

    greet product api

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App";
 // import Product Provider
 import ProductProvider from "./context/ProductContext";
+// import Sidebar Provider
+import SidebarProvider from "./context/SidebarContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ProductProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ProductProvider>
+  <SidebarProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ProductProvider>
+  </SidebarProvider>
 );
