4c134c5534a2f8036a535eda5ab55dfaa691e912
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-05-20 14:48:22 +0700

    addtocart

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,9 +3,11 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 // import react-icons
 import { BsPlus, BsEyeFill } from "react-icons/bs";
+import { CartContext } from "../context/CartContex";
 
 function Product({ product }) {
   const { id, image, category, title, price } = product;
+  const { addToCart } = useContext(CartContext);
   return (
     <>
       <div>
@@ -24,7 +26,10 @@ function Product({ product }) {
           <div className="absolute top-6 -right-11 group-hover:right-5  p-2 flex flex-col justify-center items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
             <button>
               <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500">
-                <BsPlus className="text-3xl" />
+                <BsPlus
+                  onClick={() => addToCart(product, id)}
+                  className="text-3xl"
+                />
               </div>
             </button>
             <Link
--- src/context/CartContex.jsx
+++ src/context/CartContex.jsx
@@ -6,8 +6,34 @@ const CartProvider = ({ children }) => {
   // cart state
   const [cart, setCart] = useState([]);
 
+  // add cart
+  const addToCart = (product, id) => {
+    const newItem = { ...product, amount: 1 };
+
+    // check if the item is already in the cart
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+
+    // if cart item is already in the cart
+    if (cartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount + 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
+    console.log(cart);
+
+    console.log(`${product.title} item ${id} added to cart`);
+  };
   return (
-    <CartContext.Provider value={"this is the cart context"}>
+    <CartContext.Provider value={{ addToCart }}>
       {children}
     </CartContext.Provider>
   );
