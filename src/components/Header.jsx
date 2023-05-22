b3b8b7616a49958b0ba6ac8dc3b69eb2ae1a406a
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-05-22 12:10:28 +0700

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SidebarContext } from "../context/SidebarContext";
 import { BsBag } from "react-icons/bs";
 import { CartContext } from "../context/CartContex";
@@ -7,12 +7,24 @@ import Logo from "../img/logo.svg";
 
 function Header() {
   const { isOpen, setIsOpen } = useContext(SidebarContext);
-
   const { itemAmount } = useContext(CartContext);
+
+  // header state
+  const [isActive, setIsActive] = useState(false);
+
+  // event listener
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
+    });
+  });
 
   return (
     <>
-      <header className="bg-pink-200">
+      <header
+        className={`${
+          isActive ? "bg-white py-4 shadow-md" : "bg-none py-6 "
+        } fixed w-full z-10 transition-all `}>
         <div className="container mx-auto flex items-center justify-between h-full">
           {/* Logo */}
           <Link to={"/"}>
@@ -23,7 +35,7 @@ function Header() {
 
           {/* cart */}
           <div
-            className="cursor-pointer flex relative max-w-[50px]"
+            className="cursor-pointer flex relative"
             onClick={() => setIsOpen(!isOpen)}>
             <BsBag className="text-2xl" />
             <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center">
